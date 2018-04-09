366e9e00d2a83f2c0e28aec21baae6faad780a3d
Loaiaboelsooud <Loaiaboelsooud@gmail.com>
2018-04-09 21:06:40 +0200

    Added 2 locations in a map

--- app/src/main/java/com/example/loaiaboelsooud/Spotless/SPMapFragment.java
+++ app/src/main/java/com/example/loaiaboelsooud/Spotless/SPMapFragment.java
@@ -3,37 +3,45 @@ package com.example.loaiaboelsooud.Spotless;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class SPMapFragment extends Fragment implements OnMapReadyCallback {
-
-
+    private static final float DEFAULT_ZOOM = 15f;
+    private GoogleMap mMap;
+//TODO: Add route between 2 locations
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_spmap, container, false);
         super.onCreate(savedInstanceState);
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.spmap);
-        mapFragment.getMapAsync(this);
+        initMap();
         return rootView;
     }
+
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.spmap);
+        mapFragment.getMapAsync(this);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
+        mMap = googleMap;
         LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney2 = new LatLng(-33.802, 151.211);
+        Marker(sydney);
+        Marker(sydney2);
     }
 
     public SPMapFragment() {
@@ -41,5 +49,14 @@ public class SPMapFragment extends Fragment implements OnMapReadyCallback {
     }
 
 
+    private void Marker(LatLng currentLocation) {
+        mMap.addMarker(new MarkerOptions().position(currentLocation).draggable(true));
+        moveCamera(currentLocation, DEFAULT_ZOOM);
 
+    }
+
+    private void moveCamera(LatLng latLng, float zoom) {
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
 }
